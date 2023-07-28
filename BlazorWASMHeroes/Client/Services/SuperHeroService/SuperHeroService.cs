5f2671b18bab324177624145792a2c14c25dd058
AngelikaMann <angelika0769@gmail.com>
2023-07-28 10:15:04 +0200

    Add project files.

---
+++ BlazorWASMHeroes/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorWASMHeroes.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
+
+        public SuperHeroService(HttpClient http, NavigationManager navigationManager)
+        {
+            _http = http;
+            _navigationManager = navigationManager;
+        }
+
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public async Task GetComics()
+        {
+            var result = await _http.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+            if (result != null)
+                Comics = result; ;
+        }
+
+        public async Task<SuperHero> GetSingleHero(int id)
+        {
+            var result = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+            if (result != null)
+                return result;
+            throw new Exception("Hero not found!");
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if (result != null)
+                Heroes = result;
+        }
+
+        //public Task CreateHero(SuperHero hero)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task UpdateHero(SuperHero hero)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task DeleteHero(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
